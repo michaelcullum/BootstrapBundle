4baf380f94b906a1c7a738f41f556a1d6db649e1
Unknown Bliss <m@michaelcullum.com>
2012-12-04 20:04:06 +0000

    Some stuff

---
+++ UnknownblissBootstrapBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @package Unknown Bliss Bootstrap Bundle
+ * @copyright (c) 2012 Michael Cullum (Unknown Bliss of http://michaelcullum.com)
+ * @license MIT
+ * 
+ */
+
+namespace Unknownbliss\BootstrapBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UnknownblissBootstrapBundle extends Bundle
+{
+}
